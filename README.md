75a069e99b33638d45dc6129fcb509e559be9b8f
brimo <nuralem.hdu@yandex.ru>
2024-04-12 23:34:14 +0500

    added some files

---
+++ README.md
@@ -0,0 +1,35 @@
+# Welcome to the Unitree Go2 Omniverse Project!
+
+
+## Project RoadMap:
+1. PPO balancing algorithm :white_check_mark: 
+2. Keyboard real time control :white_check_mark: 
+3. Ros2 full integration
+
+## Your feedback and support mean the world to us. 
+
+If you're as enthusiastic about this project as we are, please consider giving it a :star: star on our GitHub repository. 
+
+Your encouragement fuels our passion and helps us develop our RoadMap further. We welcome any help or suggestions you can offer!
+
+Together, let's push the boundaries of what's possible with the Unitree Go2 and ROS2!
+
+
+## System requirements
+You need to install Ubuntu 20.04 with Nvidia Isaac Sim and Nvidia Orbit
+
+
+## Usage
+
+```
+conda activate orbit
+python main.py
+```
+
+## Development
+
+To contribute or modify the project, refer to these resources for implementing additional features or improving the existing codebase. PRs are welcome!
+
+## License
+
+This project is licensed under the BSD 2-clause License - see the [LICENSE](https://github.com/abizovnuralem/go2_ros2_sdk/blob/master/LICENSE) file for details.
--- main.py
+++ main.py
@@ -84,6 +84,8 @@ import omni.appwindow  # Contains handle to keyboard
 import omni.isaac.orbit.terrains as terrain_gen
 from terrain_generator_cfg import TerrainGeneratorCfg
 
+from typing import Literal
+
 
 base_command = [0, 0, 0]
 
@@ -301,6 +303,49 @@ class CurriculumCfg:
 
 
 
+@configclass
+class ViewerCfg:
+    """Configuration of the scene viewport camera."""
+
+    eye: tuple[float, float, float] = (7.5, 7.5, 7.5)
+    """Initial camera position (in m). Default is (7.5, 7.5, 7.5)."""
+
+    lookat: tuple[float, float, float] = (0.0, 0.0, 0.0)
+    """Initial camera target position (in m). Default is (0.0, 0.0, 0.0)."""
+
+    cam_prim_path: str = "/OmniverseKit_Persp"
+    """The camera prim path to record images from. Default is "/OmniverseKit_Persp",
+    which is the default camera in the viewport.
+    """
+
+    resolution: tuple[int, int] = (1920, 1080)
+    """The resolution (width, height) of the camera specified using :attr:`cam_prim_path`.
+    Default is (1280, 720).
+    """
+
+    origin_type: Literal["world", "env", "asset_root"] = "world"
+    """The frame in which the camera position (eye) and target (lookat) are defined in. Default is "world".
+
+    Available options are:
+
+    * ``"world"``: The origin of the world.
+    * ``"env"``: The origin of the environment defined by :attr:`env_index`.
+    * ``"asset_root"``: The center of the asset defined by :attr:`asset_name` in environment :attr:`env_index`.
+    """
+
+    env_index: int = 0
+    """The environment index for frame origin. Default is 0.
+
+    This quantity is only effective if :attr:`origin` is set to "env" or "asset_root".
+    """
+
+    asset_name: str | None = None
+    """The asset name in the interactive scene for the frame origin. Default is None.
+
+    This quantity is only effective if :attr:`origin` is set to "asset_root".
+    """
+
+
 
 @configclass
 class LocomotionVelocityRoughEnvCfg(RLTaskEnvCfg):
@@ -308,6 +353,8 @@ class LocomotionVelocityRoughEnvCfg(RLTaskEnvCfg):
 
     # Scene settings
     scene: MySceneCfg = MySceneCfg(num_envs=4096, env_spacing=2.5)
+
+    viewer: ViewerCfg = ViewerCfg()
     # Basic settings
     observations: ObservationsCfg = ObservationsCfg()
     actions: ActionsCfg = ActionsCfg()
@@ -384,6 +431,10 @@ def _sub_keyboard_event(event, *args, **kwargs) -> bool:
             base_command = [0, 1, 0]
         if event.input.name == 'D':
             base_command = [0, -1, 0]
+        if event.input.name == 'Q':
+            base_command = [0, 0, 1]
+        if event.input.name == 'E':
+            base_command = [0, 0, -1]
         
     elif event.type == carb.input.KeyboardEventType.KEY_RELEASE:
         base_command = [0, 0, 0]
