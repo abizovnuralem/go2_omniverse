4e215727393b4f57a4b5807527723b97850bc917
Tamas Foldi <tfoldi@xsi.hu>
2024-05-17 19:13:28 +0200

    refactor: remove old workspace

--- IsaacSim-ros_workspaces-old/noetic_ws/src/navigation/isaac_ros_navigation_goal/isaac_ros_navigation_goal/goal_generators/__init__.py
+++
@@ -1,2 +0,0 @@
-from .random_goal_generator import RandomGoalGenerator
-from .goal_reader import GoalReader
--- IsaacSim-ros_workspaces-old/noetic_ws/src/navigation/isaac_ros_navigation_goal/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=["goal_generators", "obstacle_map"], package_dir={"": "isaac_ros_navigation_goal"}
-)
-setup(**d)
